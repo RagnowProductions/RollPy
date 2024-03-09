3b4771f8c17b87fb0a7cfcb4c21d5117a15af450
Fenn <nicolaileff@icloud.com>
2024-03-09 14:37:49 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,1 +1,14 @@
-
+uname = input("WHAT IS YOUR NAME, ADVENTURER?")
+questSelection = int(input("WHAT IS YOUR QUEST? (1 - TO FIND THE SECRETS, 2 - TO GET AS MUCH GOLD AS POSSIBLE, 3 - TO GET TROLLED MASSIVELY)"))
+irrelevant-inbox92687_26381.62292/1 = print("WHAT IS... wait i forgot")
+print("Anyways, move along now, " + uname +". Dangers lead up ahead.")
+if questSelection == 1:
+  print('You find a hidden walkway with a sign that says "BEWARE ' + uname + ', THE SECRETS ARE TOO SCARY"')
+  wordWritten = input("What will you respond with on the sign? (this cannot be skipped its important to the LORE ¯\_(ツ)_/¯ )")
+  print('You wrote "' + wordWritten + '" on the sign.')
+elif questSelection == 2:
+  print("You find piles of gold. Mountains of gold. But you can only carry 10 tons of gold at a time with the help of some friends. At the moment, you have no friends, so you can only carry 1/3 of that gold.")
+  whatdoyoudo = input("What will you do? (1 - Wait in the gold room, 2 - Climb the gold tower, 3 - Follow the strange hallway leading to the room.)")
+elif questSelection == 3:
+  print("You find yourself in a dark room with a small hallway boarded up. You feel a sense of impending doom. Suddenly, 3 AI robots, with pictures of a rickroll, a troll face, and a cat, fall from the ceiling chasing after you with deadly lasers.")
+  whatdoyoudo = ("What will you do? (1 - Break the hallway barrier, 2 - Grab a mace from the corner (where did that come from?) and destroy the robots, 3 - Throw the robots against the wall and pin them down.)")
